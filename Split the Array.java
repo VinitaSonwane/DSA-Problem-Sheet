398f8157d9fbc08ac9b958a3d17017d6994a8312
Vinita Santosh Sonwane <121440798+VinitaSonwane@users.noreply.github.com>
2024-02-25 09:24:25 +0530

    Create Split the Array.java

---
+++ Split the Array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPossibleToSplit(int[] nums) {
+       
+        HashMap<Integer, Integer> countMap = new HashMap<>();
+        for (int num : nums) {
+            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
+        }
+        
+        
+        for (int count : countMap.values()) {
+            if (count > 2) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
