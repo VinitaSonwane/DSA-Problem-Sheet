371fa9ede238e47225a2c0bbce26f15996d38148
Vinita Santosh Sonwane <121440798+VinitaSonwane@users.noreply.github.com>
2024-03-16 08:45:05 +0530

    Create Delete without head pointer(GFG).java

---
+++ Delete without head pointer(GFG).java
@@ -0,0 +1,14 @@
+class Solution
+{
+    void deleteNode(Node del_node)
+    {
+         // Your code here
+         if(del_node==null || del_node.next==null){
+             return ;
+    }
+         
+         del_node.data = del_node.next.data ; 
+         
+         del_node.next = del_node.next.next;
+         }
+}
